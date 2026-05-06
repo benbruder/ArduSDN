372f1ff1c2ad2eaa1f8f5324cc428349cffe62e2
Ben Bruder <ben@benbruder.com>
2026-05-06 02:08:40 -0400

    lot of inits

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include <Arduino.h>
+
+#if defined(ROLE_MEGA2_CONTROLLER)
+  #include "controller.h"
+#elif defined(ROLE_MEGA1_RELAY)
+  #include "relaymega.h"
+#elif defined(ROLE_NANO_SWITCH)
+  #include "nanoswitch.h"
+#elif defined(ROLE_UNO_HOST)
+  #include "unohost.h"
+#else
+  #error "No role selected in platformio.ini"
+#endif
+
+void setup() {
+  // roleSetup();
+}
+
+void loop() {
+  // roleLoop();
+}
